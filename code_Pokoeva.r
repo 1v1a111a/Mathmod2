aa7b00454592af2a982a650b6731a318c40254c2
1v1a111a <36400705+1v1a111a@users.noreply.github.com>
2018-03-28 08:46:15 +0300

    Update code_Pokoeva.r

--- code_Pokoeva.r
+++ code_Pokoeva.r
@@ -5,14 +5,17 @@ library("dplyr")
 library("tibble") 
 library("readr") 
 class(eddypro)
+#загружаем файл,считываем
 eddypro=read_csv("C:/Pokoeva_125/MathMod/eddypro.csv", skip = 1,na=c("","NA","-9999","-9999.0"),comment =c("["))
 View(eddypro)
 eddypro=eddypro [-1,]
 eddypro
 glimpse(eddypro)
+#удаляем лишние колонки
 eddypro = select(eddypro, -(roll))
 eddypro<-eddypro[,c(-1,-3,-9,-12,-15,-18,-21,-30,-35,-70,-88:-99)] 
 eddypro = eddypro %>% mutate_if(is.character, factor)
+#Убираем проблему со знаками в переменных 
 names(eddypro)= str_replace_all(names(eddypro), "[!]","_emph_")
 names(eddypro) = names(eddypro) %>% str_replace_all("[!]","_emph_") %>%
   str_replace_all("[?]","_quest_") %>%  
@@ -29,20 +32,32 @@ names(eddypro) = names(eddypro) %>% str_replace_all("[!]","_emph_") %>%
   str_replace_all("[()]","_") 
 glimpse(eddypro) 
 sapply(eddypro,is.numeric) 
+#оставляем численные данные
 eddypro_numeric = eddypro[,sapply(eddypro,is.numeric)]
 eddypro_non_numeric = eddypro[,!sapply(eddypro,is.numeric) ] 
+#меняем тип колонки
 eddypro$daytime = as.logical(eddypro$daytime) 
-night =eddypro<-eddypro[eddypro$DOY>133 & eddypro$DOY<150 & eddy$daytime == FALSE, c(1:ncol(eddy))] 
+#выбираем весенний ночной период
+night =eddypro<-eddypro[eddypro$DOY>133 & eddypro$DOY<150 & eddy$daytime == FALSE, c(1:ncol(eddypro))] 
+#проводим корреляцию
 cor_td = cor(eddypro_numeric) 
 cor_td 
 cor_td = cor(drop_na(eddypro_numeric)) %>% as.data.frame %>% select(h2o_flux) 
 vars = row.names(cor_td)[cor_td$h2o_flux^2 > .1] %>% na.exclude 
 vars 
+ #Собираем все переменные в одну формулу 
 formula=as.formula(paste("h2o_flux~",paste(vars,collapse="+"),sep = ""))
 formula
+#создаем обучающую выборку
+row_numbers = 1:length(eddypro$date) 
+teach = sample(row_numbers, floor(length(eddypro$date)*.7)) 
+test = row_numbers[-teach] 
+teaching_eddypro_unq = eddypro[teach,] 
+testing_eddypro_unq = eddypro[test,] 
+#взаимодействие переменных
 model1=lm((h2o_flux ~ DOY + Tau + rand_err_Tau + H + rand_err_H + LE + rand_err_LE + co2_flux + rand_err_co2_flux + h2o_flux + rand_err_h2o_flux + H_strg + co2_molar_density + h2o_time_lag + sonic_temperature +  air_temperature + air_density + air_molar_volume + es + RH + VPD + u_star_ + TKE + T_star_ + un_Tau + un_H + un_LE + un_co2_flux + un_h2o_flux + mean_value + u_var + v_var + w_var + h2o_var +  w_div_ts_cov + w_div_co2_cov + w_div_h2o_cov + co2_signal_strength_7200 +  h2o_signal_strength_7200 + flowrate)^2,data=eddypro)
-coef(model1) 
-resid(model1) 
+coef(model1) #вытаскиваем коэффииенты из модели
+resid(model1) #вытаскиваем остатки
 confint(model1) 
 summary(model1)
 anova(model1)
@@ -53,8 +68,8 @@ confint(model2)
 summary(model2)
 anova(model2)
 model3 =lm((h2o_flux ~ DOY + Tau + H + LE + co2_flux + h2o_flux + co2_molar_density  +  air_temperature + air_density + air_molar_volume es + RH + VPD + u. + TKE + T )^2-Tau:H -Tau:LE -Tau:co2_flux - Tau:co2_molar_density-Tau:H_strg-Tau:h2o_time_lag-Tau:air_density -  Tau:air_molar_volume-Tau:u.-Tau:T.-H:LE-H:co2_molar_density -H:h2o_time_lag -H:air_density-H_strg:es-H_strg:RH-H_strg:VPD-H_strg:u.-co2_molar_density:h2o_time_lag - co2_molar_density:u.-co2_molar_density:T.-h2o_time_lag:u. -h2o_time_lag:T.-sonic_temperature:RH -air_density:VPD-air_density:u.-air_molar_volume:u.-es:RH-air_molar_volume:es ,data=eddypro) 
-coef(model3)#вытаскиваем коэффииенты из модели
-resid(model3)  #вытаскиваем остатки
+coef(model3)
+resid(model3)  
 confint(model3) 
 summary(model3)
 anova(model3)
