ec3a8ce5c55e2b60e126e55bb61ad40b15f2e1ec
1v1a111a <36400705+1v1a111a@users.noreply.github.com>
2018-03-28 09:29:51 +0300

    Update code_Pokoeva.r

--- code_Pokoeva.r
+++ code_Pokoeva.r
@@ -62,20 +62,7 @@ resid(model1) #вытаскиваем остатки
 confint(model1) 
 summary(model1)
 anova(model1)
-model2=lm((h2o_flux ~ DOY + Tau + H + LE + co2_flux + h2o_flux + co2_molar_density  +  air_temperature + air_density + air_molar_volume es + RH + VPD + u. + TKE + T )^2,data=eddypro)
-model2
-coef(model2) 
-resid(model2) 
-confint(model2) 
-summary(model2)
-anova(model2)
-model3 =lm((h2o_flux ~ DOY + Tau + H + LE + co2_flux + h2o_flux + co2_molar_density  +  air_temperature + air_density + air_molar_volume es + RH + VPD + u. + TKE + T )^2-Tau:H -Tau:LE -Tau:co2_flux - Tau:co2_molar_density-Tau:H_strg-Tau:h2o_time_lag-Tau:air_density -  Tau:air_molar_volume-Tau:u.-Tau:T.-H:LE-H:co2_molar_density -H:h2o_time_lag -H:air_density-H_strg:es-H_strg:RH-H_strg:VPD-H_strg:u.-co2_molar_density:h2o_time_lag - co2_molar_density:u.-co2_molar_density:T.-h2o_time_lag:u. -h2o_time_lag:T.-sonic_temperature:RH -air_density:VPD-air_density:u.-air_molar_volume:u.-es:RH-air_molar_volume:es ,data=eddypro) 
-model3
-coef(model3)
-resid(model3)  
-confint(model3) 
-summary(model3)
-anova(model3)
+
 
 
 
